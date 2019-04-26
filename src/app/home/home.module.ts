eedcb660510f046ce6a28efc92ce87fdf3877278
jesus.javega <jesusgraficap@gmail.com>
2019-04-27 00:29:01 +0200

    Component guide card

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,23 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
+import { IonicModule } from '@ionic/angular'
+import { RouterModule } from '@angular/router'
 
-import { HomePage } from './home.page';
+import { HomePage } from './home.page'
+import { GuidePreviewCardModule } from '../../shared/components/guide-preview-card/guidePreviewCard.module'
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    GuidePreviewCardModule,
     RouterModule.forChild([
       {
         path: '',
-        component: HomePage
-      }
-    ])
+        component: HomePage,
+      },
+    ]),
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
 })
 export class HomePageModule {}
---
+++ src/shared/components/guide-preview-card/guidePreviewCard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+/**
+ * @title Card with multiple sections
+ */
+@Component({
+  selector: 'app-guide-preview-card',
+  templateUrl: 'guidePreviewCard.component.html',
+  styleUrls: ['guidePreviewCard.component.scss'],
+})
+export class GuidePreviewCardComponent {}
