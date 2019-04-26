9581c455b4ac6391bc948a01a096bd26305a04f0
jesus.javega <jesusgraficap@gmail.com>
2019-04-26 13:18:51 +0200

    prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,39 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from '@ionic/angular'
+import { SplashScreen } from '@ionic-native/splash-screen/ngx'
+import { StatusBar } from '@ionic-native/status-bar/ngx'
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
 })
 export class AppComponent {
   public appPages = [
     {
       title: 'Home',
       url: '/home',
-      icon: 'home'
+      icon: 'home',
     },
     {
       title: 'List',
       url: '/list',
-      icon: 'list'
-    }
-  ];
+      icon: 'list',
+    },
+  ]
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
-    this.initializeApp();
+    this.initializeApp()
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+      this.statusBar.styleDefault()
+      this.splashScreen.hide()
+    })
   }
 }
