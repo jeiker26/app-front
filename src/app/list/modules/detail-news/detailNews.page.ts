489326fde7c5b7bcdc1a597384e8b76a92e6d16a
jesus.javega <jesusgraficap@gmail.com>
2019-05-22 12:19:29 +0200

    clean and create compoent detail

---
+++ src/app/list/modules/detail-news/detailNews.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-news',
+  templateUrl: 'detailNews.page.html',
+  styleUrls: ['detailNews.page.scss'],
+})
+export class DetailNewsPage {
+  constructor() {}
+}
